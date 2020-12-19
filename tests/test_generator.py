2063847fcd75b1f83bd055924ce490ffdd962011
root <root@localhost.localdomain>
2020-12-19 05:31:03 -0500

    Improved syntax generator.py & test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -2,20 +2,22 @@ import unittest
 
 from buzz import generator
 
+
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+    words = ('foo', 'bar', 'foobar')
+    word = generator.sample(words)
+    assert word in words
+
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    words = ('foo', 'bar', 'foobar')
+    word = generator.sample(l, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
-    assert words[0] is not words[1]
+    assert word[0] in words
+    assert word[1] in words
+    assert word[0] is not word[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
-
